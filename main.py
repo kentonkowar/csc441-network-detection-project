f121f56932e7a86423f01f18f3dc84fed63fcc8a
Kenton Kowar <kentonkowar@gmail.com>
2025-11-17 12:33:25 -0700

    main outline init

---
+++ main.py
@@ -0,0 +1,18 @@
+from sklearn.ensemble import IsolationForest
+import pandas as pd
+import sys
+
+
+def read_dataset(file: str) -> pd.DataFrame:
+
+    df = pd.DataFrame()
+    return df
+
+def issolation_forest(df: pd.DataFrame):
+
+    return
+
+
+if __name__ == "__main__":
+
+    exit(0)
